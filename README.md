dd5003de2b973944d5c978bbd9ef32af8d771056
Simon Prickett <simon@crudworks.org>
2015-08-18 22:30:12 -0700

    Added more events for online/offline and geolocation.

--- README.md
+++ README.md
@@ -21,15 +21,17 @@ To use, clone the repo then:
 * Open up a Terminal and:
     * cd <cloned_repo_location>/scripts
     * ./post_clone.sh
-    * Runs a one off script that adds the Cordova platform files for Android and iOS, then installs the following boiler plate plugins that you are pretty much always going to need:
+    * Runs a one off script that adds the Cordova platform files for Android and iOS, then installs the following boilerplate plugins that you are pretty much always going to need:
         * console
         * geolocation
         * splashscreen
+		* device
+		* network-information
 * Then:
     * cd ../app
     * cordova build ios
     * cordova build android
-* You should then see a generic Hello World! app that checks for Cordova's deviceready event
+* You should then see a generic Hello World! app that checks for Cordova's deviceready event and asks for permission to determine your geolocation
 
 The app/resources folder contains:
 
@@ -50,3 +52,13 @@ The app/www folder contains:
 Notes:
 
 * This boilerplate is for an app in portrait orientation, which is set in app/config.xml and also in app/www/js/app.js where there is a handler to stop iOS devices from letting the app rotate to "upside down" portrait
+* There are basic event handlers for common app lifecycle events that just log to the console:
+    * Offline (lost network)
+	* Online (regained network)
+	* Geolocation obtained (got a position fix)
+	* Geolocation error (failed to get a position fix)
+	* Pause (app sent to background)
+	* Resume (app brought to foreground)
+	* Back button pressed (Android only)
+	* Menu button pressed (Android only)
+	* Search button pressed (Android only)
--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,15 +1,59 @@
 var app = {
 	initialize: function() {
-		this.bindEvents();
-	},
-
-	bindEvents: function() {
 		document.addEventListener('deviceready', this.onDeviceReady, false);
 	},
 
 	onDeviceReady: function() {
+		document.addEventListener('pause', app.onDevicePause, false);
+		document.addEventListener('resume', app.onDeviceResume, false);
+		document.addEventListener('offline', app.onOffline, false);
+		document.addEventListener('online', app.onOnline, false);
+
+		if (device.platform === 'Android') {
+			document.addEventListener('backbutton', app.onBackButton, false);
+			document.addEventListener('menubutton', app.onMenuButton, false);
+			document.addEventListener('searchbutton', app.onSearchButton, false);
+		}
+
 		$('#app').append('<div class="row"><div class="col-lg-12 text-center"><h2>Device is Ready!</h2></div></div>');
 		navigator.splashscreen.hide();
+		navigator.geolocation.getCurrentPosition(app.onGeolocationSuccess, app.onGeolocationError);
+	},
+
+	onGeolocationSuccess: function(position) {
+		console.log(position);
+	},
+
+	onGeolocationError: function(error) {
+		console.log(error);
+	},
+
+	onDevicePause: function() {
+		console.log('App went into the background.');
+	},
+
+	onDeviceResume: function() {
+		console.log('App was brought back to the foreground.');
+	},
+
+	onOnline: function() {
+		console.log('Device got a network connection.');
+	},
+
+	onOffline: function() {
+		console.log('Device lost network connection.');
+	},
+
+	onBackButton: function() {
+		console.log('User pressed back button.');
+	},
+
+	onMenuButton: function() {
+		console.log('User pressed menu button.');
+	},
+
+	onSearchButton: function() {
+		console.log('User pressed search button.');
 	}
 }
 
